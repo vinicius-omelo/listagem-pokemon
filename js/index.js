3bc52a12805ed458cf03d9d913d653d899ce8c38
vinicius-omelo <viniciuss.uee@gmail.com>
2023-05-23 10:15:36 -0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,20 @@
+const botaoAlterarTema = document.getElementById
+    ("botao-alterar-tema");
+
+const body = document.querySelector("body");
+
+const imagemBotaoTrocaDeTema = document.querySelector(".imagem-botao")
+
+botaoAlterarTema.addEventListener("click", () => {
+
+    body.classList.toggle("modo-escuro");
+
+    const modoEscuroEstaAtivo = body.classList.contains("modo-escuro");
+
+    if (modoEscuroEstaAtivo) {
+        imagemBotaoTrocaDeTema.setAttribute("src", "./image/moon.png");
+    }
+    else {
+        imagemBotaoTrocaDeTema.setAttribute("src", "./image/sun.png");
+    }
+});
